dbb17f232102019a037d791d1adb29527ffadb90
Fsploit (retarded as fuck) <169143383+Fsploit@users.noreply.github.com>
2024-09-09 07:03:25 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,12 @@
+if executed then
+    local scriptContent = game:HttpGet("https://raw.githubusercontent.com/Fsploit/Frost/main/F-r-o-s-t-w-a-r-e")
+    local success, err = pcall(function()
+        loadstring(scriptContent)()
+    end)
 
+    if success then
+        print("loaded Frostware.")
+    else
+        print("Executer not supported : " .. err)
+    end
+end
